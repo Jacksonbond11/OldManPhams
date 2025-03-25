8b31d936e3a10ca639a66581585064cc15d290f2
Jackson Bond <jacksonbond11@yahoo.com>
2025-03-24 19:10:09 -0500

    Fixing typo

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -9,11 +9,11 @@ const Services = () => {
       <div className="border-b-2"></div>
       <div className="md:flex justify-content-between max-w-5xl m-auto mt-6 p-2 items-center text-center md:text-left">
         <div className="md:m-0 mt-2 text-lg">
-          <ul className="list-disc font-bold">
+          <ul className="md:list-disc font-bold">
             <li className="mt-4">Lawn Mowing</li>
             <li className="mt-4">Trimming</li>
             <li className="mt-4">Edging</li>
-            <li className="mt-4">Lead Blowing</li>
+            <li className="mt-4">Leaf Blowing</li>
             <li className="mt-4">Flowerbed Maintenance & Design</li>
           </ul>
         </div>
